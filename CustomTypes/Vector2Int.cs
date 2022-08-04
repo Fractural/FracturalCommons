b4d76d95960d886320c08ca4afef8bb60a4e8c80
Atlinx <atlinuxtm@gmail.com>
2022-08-04 07:43:02 +0000

    Update release to output generated at bd27aca

--- CustomTypes/Vector2Int.cs
+++ CustomTypes/Vector2Int.cs
@@ -1,31 +1,83 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Godot;
 
 namespace Fractural
 {
-    public struct Vector2Int
-    {
-        public Vector2Int(int x, int y)
-        {
-            X = x;
-            Y = y;
-        }
-
-        public int X { get; set; }
-        public int Y { get; set; }
-
-        public static implicit operator Vector2(Vector2Int vector)
-        {
-            return new Vector2(vector.X, vector.Y);
+	public struct Vector2Int
+	{
+		public Vector2Int(int x, int y)
+		{
+			X = x;
+			Y = y;
 		}
 
-        public static implicit operator Vector2Int(Vector2 vector)
-        {
-            return new Vector2Int((int) vector.x, (int) vector.y);
+		public int X { get; set; }
+		public int Y { get; set; }
+
+		public static implicit operator Vector2(Vector2Int vector)
+		{
+			return new Vector2(vector.X, vector.Y);
 		}
-    }
+
+		public static implicit operator Vector2Int(Vector2 vector)
+		{
+			return new Vector2Int((int)vector.x, (int)vector.y);
+		}
+
+		public static Vector2Int operator +(Vector2Int a, Vector2Int b)
+		{
+			return new Vector2Int(a.X + b.X, a.Y + b.Y);
+		}
+
+		public static Vector2Int operator -(Vector2Int a)
+		{
+			return a * -1;
+		}
+
+		public static Vector2Int operator -(Vector2Int a, Vector2Int b)
+		{
+			return a + -b;
+		}
+
+		public static Vector2Int operator *(Vector2Int a, int b)
+		{
+			return new Vector2Int(a.X * b, a.Y * b);
+		}
+
+		public static Vector2Int operator *(Vector2Int a, float b)
+		{
+			return new Vector2Int(Mathf.RoundToInt(a.X * b), Mathf.RoundToInt(a.Y * b));
+		}
+
+		public static Vector2Int operator /(Vector2Int a, int b)
+		{
+			return new Vector2Int(a.X / b, a.Y / b);
+		}
+
+		public static Vector2Int operator /(Vector2Int a, float b)
+		{
+			if (b == 0f) throw new DivideByZeroException();
+			return new Vector2Int(Mathf.RoundToInt(a.X / b), Mathf.RoundToInt(a.Y / b));
+		}
+
+		public static bool operator ==(Vector2Int a, Vector2Int b)
+		{
+			return a.X == b.X && a.Y == b.Y;
+		}
+
+		public static bool operator !=(Vector2Int a, Vector2Int b)
+		{
+			return !(a == b);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return obj is Vector2Int otherVec && this == otherVec;
+		}
+
+		public override string ToString()
+		{
+			return "(" + X + "," + Y + ")";
+		}
+	}
 }
