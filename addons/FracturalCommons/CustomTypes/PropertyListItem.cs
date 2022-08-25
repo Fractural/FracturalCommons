c4578fb9428c8a15faff0cb03a123422971a9355
Atlinx <atlinuxtm@gmail.com>
2022-08-25 17:16:26 -0400

    Add property list item

---
+++ addons/FracturalCommons/CustomTypes/PropertyListItem.cs
@@ -0,0 +1,32 @@
+﻿using Godot;
+
+namespace Fractural
+{
+    public struct PropertyListItem
+    {
+        public string name;
+        public Variant.Type type;
+        public PropertyHint hint;
+        public string hintString;
+        public PropertyUsageFlags usage;
+
+        public PropertyListItem(string name, Variant.Type type, PropertyHint hint = PropertyHint.None, string hintString = "", PropertyUsageFlags usage = PropertyUsageFlags.Default)
+        {
+            this.name = name;
+            this.type = type;
+            this.hint = hint;
+            this.hintString = hintString;
+            this.usage = usage;
+        }
+
+        public static implicit operator Godot.Collections.Dictionary(PropertyListItem item)
+        {
+            var dict = new Godot.Collections.Dictionary();
+            dict["name"] = item.name;
+            dict["type"] = item.type;
+            dict["hint_string"] = item.hintString;
+            dict["usage"] = item.usage;
+            return dict;
+        }
+    };
+}
