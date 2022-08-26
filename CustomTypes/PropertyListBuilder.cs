25932e6446489e5d1639bf1df82ce4b181fbad67
Atlinx <atlinuxtm@gmail.com>
2022-08-26 00:08:34 +0000

    Update release to output generated at 301be10

--- CustomTypes/PropertyListBuilder.cs
+++ CustomTypes/PropertyListBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Godot;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Fractural
@@ -6,10 +7,17 @@ namespace Fractural
     public class PropertyListBuilder
     {
         public List<PropertyListItem> Items { get; } = new List<PropertyListItem>();
+
         public void AddItem(PropertyListItem item)
         {
             Items.Add(item);
         }
+
+        public void AddItem(string name, Variant.Type type, PropertyHint hint = PropertyHint.None, string hintString = "", PropertyUsageFlags usage = PropertyUsageFlags.Default)
+        {
+            AddItem(new PropertyListItem(name, type, hint, hintString, usage));
+        }
+
         public Godot.Collections.Array Build()
         {
             return new Godot.Collections.Array(Items.Select(x => x.ToGDDict()));
