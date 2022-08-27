42b752e520eed889468a59005e7cee5f3ed39154
Atlinx <atlinuxtm@gmail.com>
2022-08-27 13:28:59 -0400

    Add select for 2D array

--- addons/FracturalCommons/Utils/CSharpUtils.cs
+++ addons/FracturalCommons/Utils/CSharpUtils.cs
@@ -5,88 +5,99 @@ using System.Linq;
 
 namespace Fractural.Utils
 {
-    /// <summary>
-    /// Utility class for C# related functions.
-    /// </summary>
-    public static class CSharpUtils
-    {
-        public static Dictionary<string, string> CSharpScriptsDict { get; } = new Dictionary<string, string>();
+	/// <summary>
+	/// Utility class for C# related functions.
+	/// </summary>
+	public static class CSharpUtils
+	{
+		public static Dictionary<string, string> CSharpScriptsDict { get; } = new Dictionary<string, string>();
 
-        static CSharpUtils()
-        {
-            List<string> scripts = FileUtils.GetDirFiles("res://", true, new string[] { "cs" }, new string[] { ".mono" });
-            foreach (string path in scripts)
-                if (!CSharpScriptsDict.ContainsKey(path.GetFileName()))
-                    CSharpScriptsDict.Add(path.GetFileName(), path);
-        }
+		static CSharpUtils()
+		{
+			List<string> scripts = FileUtils.GetDirFiles("res://", true, new string[] { "cs" }, new string[] { ".mono" });
+			foreach (string path in scripts)
+				if (!CSharpScriptsDict.ContainsKey(path.GetFileName()))
+					CSharpScriptsDict.Add(path.GetFileName(), path);
+		}
 
-        public static string GetRelativePath<T>()
-        {
-            if (CSharpScriptsDict.TryGetValue(typeof(T).Name, out string filepath))
-                return filepath.GetBaseDir();
-            return null;
-        }
+		public static string GetRelativePath<T>()
+		{
+			if (CSharpScriptsDict.TryGetValue(typeof(T).Name, out string filepath))
+				return filepath.GetBaseDir();
+			return null;
+		}
 
-        public static T InstantiateCSharpNode<T>() where T : Node
-        {
-            if (CSharpScriptsDict.TryGetValue(typeof(T).Name, out string filepath))
-                return (T)ResourceLoader.Load<CSharpScript>(filepath).New();
-            return null;
-        }
+		public static T InstantiateCSharpNode<T>() where T : Node
+		{
+			if (CSharpScriptsDict.TryGetValue(typeof(T).Name, out string filepath))
+				return (T)ResourceLoader.Load<CSharpScript>(filepath).New();
+			return null;
+		}
 
-        public static Node InstantiateCSharpNode(Type type)
-        {
-            if (CSharpScriptsDict.TryGetValue(type.Name, out string filepath))
-                return (Node)ResourceLoader.Load<CSharpScript>(filepath).New();
-            return null;
-        }
+		public static Node InstantiateCSharpNode(Type type)
+		{
+			if (CSharpScriptsDict.TryGetValue(type.Name, out string filepath))
+				return (Node)ResourceLoader.Load<CSharpScript>(filepath).New();
+			return null;
+		}
 
-        public static int CombineHashCodes(int h1, int h2)
-        {
-            return ((h1 << 5) + h1) ^ h2;
-        }
+		public static int CombineHashCodes(int h1, int h2)
+		{
+			return ((h1 << 5) + h1) ^ h2;
+		}
 
-        public static string TrimSuffix(this string str, string trimmedString)
-        {
-            int lastIndex = str.LastIndexOf(trimmedString);
-            if (lastIndex < 0)
-                return str;
-            return str.Substring(0, lastIndex);
-        }
+		public static string TrimSuffix(this string str, string trimmedString)
+		{
+			int lastIndex = str.LastIndexOf(trimmedString);
+			if (lastIndex < 0)
+				return str;
+			return str.Substring(0, lastIndex);
+		}
 
-        public static string TrimPrefix(this string str, string trimmedString)
-        {
-            int index = str.IndexOf(trimmedString);
-            if (index < 0)
-                return str;
-            return str.Substring(index + trimmedString.Length);
-        }
+		public static string TrimPrefix(this string str, string trimmedString)
+		{
+			int index = str.IndexOf(trimmedString);
+			if (index < 0)
+				return str;
+			return str.Substring(index + trimmedString.Length);
+		}
 
-        public static string GetFileName(this string str)
-        {
-            string fileName = str.GetFile();
-            int periodIndex = fileName.Find(".");
-            if (periodIndex < 0)
-                return fileName;
-            return fileName.Substring(0, periodIndex);
-        }
+		public static string GetFileName(this string str)
+		{
+			string fileName = str.GetFile();
+			int periodIndex = fileName.Find(".");
+			if (periodIndex < 0)
+				return fileName;
+			return fileName.Substring(0, periodIndex);
+		}
 
-        /// <summary>
-        /// Gets the extension of a file path (excluding the period).
-        /// </summary>
-        /// <param name="filePath">File path as a string</param>
-        /// <returns>Extension of the file (excluding the period)</returns>
-        public static string GetExtension(this string filePath)
-        {
-            return filePath.Split('.').Last();
-        }
+		/// <summary>
+		/// Gets the extension of a file path (excluding the period).
+		/// </summary>
+		/// <param name="filePath">File path as a string</param>
+		/// <returns>Extension of the file (excluding the period)</returns>
+		public static string GetExtension(this string filePath)
+		{
+			return filePath.Split('.').Last();
+		}
 
-        public static void Populate<T>(this T[] arr, T value)
-        {
-            for (int i = 0; i < arr.Length; i++)
-            {
-                arr[i] = value;
-            }
-        }
-    }
+		public static void Populate<T>(this T[] arr, T value)
+		{
+			for (int i = 0; i < arr.Length; i++)
+			{
+				arr[i] = value;
+			}
+		}
+
+		public static R[,] Select<T, R>(this T[,] items, Func<T, R> f)
+		{
+			int d0 = items.GetLength(0);
+			int d1 = items.GetLength(1);
+			R[,] result = new R[d0, d1];
+			for (int i0 = 0; i0 < d0; i0 += 1)
+				for (int i1 = 0; i1 < d1; i1 += 1)
+					result[i0, i1] = f(items[i0, i1]);
+			return result;
+		}
+	}
 }
