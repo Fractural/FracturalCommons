d022799eecc2883adf75ea6ea090914b3bc6192b
Atlinx <atlinuxtm@gmail.com>
2023-03-09 17:29:18 +0000

    Update release to output generated at 176bb02

--- Plugin.cs
+++ Plugin.cs
@@ -8,23 +8,12 @@ namespace Fractural.Commons
     [Tool]
     public class Plugin : ExtendedPlugin
     {
-        public static class Settings
-        {
-            public const string GenerateVersionPreprocessorDefines = "Generate Version Preprocessor Defines";
-            public const string GenerateCSharpScriptsTable = "Generate CSharp Scripts Table";
-        }
-
         public override string PluginName => "FracturalCommons";
 
         protected override void Load()
         {
-            AddSetting(Settings.GenerateCSharpScriptsTable, Variant.Type.Bool, false);
-            if (GetSetting<bool>(Settings.GenerateCSharpScriptsTable))
-                CSharpScriptUtils.GenerateCSharpScriptsTable();
-
-            AddSetting(Settings.GenerateVersionPreprocessorDefines, Variant.Type.Bool, false);
-            if (GetSetting<bool>(Settings.GenerateVersionPreprocessorDefines))
-                EngineUtils.GenerateVersionPreprocessorDefines();
+            AddSubPlugin(new MainPlugin());
+            AddSubPlugin(new CSharpResourceRegistryPlugin());
         }
     }
 }
---
+++ SubPlugins/MainPlugin.cs
@@ -0,0 +1,41 @@
+﻿using Fractural.Plugin;
+using Godot;
+using System.Collections.Generic;
+using GDC = Godot.Collections;
+using System.Linq;
+using Fractural.Utils;
+
+#if TOOLS
+namespace Fractural.Commons
+{
+    public class MainPlugin : SubPlugin
+    {
+        public static class Settings
+        {
+            public static MainPlugin Plugin { get; private set; }
+
+            public static bool GenerateVersionPreprocessorDefines => Plugin.GetSetting<bool>(nameof(GenerateCSharpScriptsTable));
+            public static bool GenerateCSharpScriptsTable => Plugin.GetSetting<bool>(nameof(GenerateVersionPreprocessorDefines));
+
+            public static void Init(MainPlugin plugin)
+            {
+                Plugin = plugin;
+                Plugin.AddSetting(nameof(GenerateCSharpScriptsTable), Variant.Type.Bool, false);
+                Plugin.AddSetting(nameof(GenerateVersionPreprocessorDefines), Variant.Type.Bool, false);
+            }
+        }
+
+        public override string PluginName => "Main";
+
+        public override void EnablePlugin()
+        {
+            Settings.Init(this);
+
+            if (Settings.GenerateCSharpScriptsTable)
+                CSharpScriptUtils.GenerateCSharpScriptsTable();
+            if (Settings.GenerateVersionPreprocessorDefines)
+                EngineUtils.GenerateVersionPreprocessorDefines();
+        }
+    }
+}
+#endif
