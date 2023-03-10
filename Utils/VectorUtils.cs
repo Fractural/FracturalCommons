32f4cb8dc73ed935b70adbbfc0e9c3d96189d9eb
Atlinx <atlinuxtm@gmail.com>
2023-03-10 16:32:02 +0000

    Update release to output generated at 4c175c6

--- Utils/VectorUtils.cs
+++ Utils/VectorUtils.cs
@@ -4,10 +4,10 @@ namespace Fractural.Utils
 {
 	public static class VectorUtils
 	{
-		private static float DistanceToLine(this Vector2 point, Vector2 lineStart, Vector2 lineEnd)
+		public static float DistanceToLine(this Vector2 point, Vector2 lineStart, Vector2 lineEnd)
 			=> Mathf.Sqrt(DistanceToLineSquared(point, lineStart, lineEnd));
 
-		private static float DistanceToLineSquared(this Vector2 point, Vector2 lineStart, Vector2 lineEnd)
+		public static float DistanceToLineSquared(this Vector2 point, Vector2 lineStart, Vector2 lineEnd)
 		{
 			float l2 = lineStart.DistanceSquaredTo(lineEnd);
 			if (l2 == 0) return lineStart.DistanceSquaredTo(point);
