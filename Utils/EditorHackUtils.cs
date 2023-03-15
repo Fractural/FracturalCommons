a7e918e64986ef7d11de99cd32f2bd2c9d9462fb
Atlinx <atlinuxtm@gmail.com>
2023-03-15 01:05:45 +0000

    Update release to output generated at 31bcbe3

--- Utils/EditorHackUtils.cs
+++ Utils/EditorHackUtils.cs
@@ -3,44 +3,57 @@ using Godot;
 
 namespace Fractural.Utils
 {
-	/// <summary>
-	/// Utils for peeking around the Godot editor, such
-	/// as digging through the tree.
-	/// </summary>
-	public static class EditorHackUtils
-	{
-		public delegate bool PrintTreeCondition(Node node);
-		public delegate void WalkTreeCallback(Node node);
+    /// <summary>
+    /// Utils for peeking around the Godot editor, such
+    /// as digging through the tree.
+    /// </summary>
+    public static class EditorHackUtils
+    {
+        public delegate bool PrintTreeCondition(Node node);
+        public delegate void WalkTreeCallback(Node node);
 
-		public static void WalkFromTop(Node node, WalkTreeCallback callback)
-		{
-			// Get root node
-			while (node.GetParent() != null)
-			{
-				node = node.GetParent();
-			}
-			WalkTree(node, callback);
-		}
+        public static void WalkFromTop(Node node, WalkTreeCallback callback)
+        {
+            // Get root node
+            while (node.GetParent() != null)
+            {
+                node = node.GetParent();
+            }
+            WalkTree(node, callback);
+        }
 
-		public static void WalkTree(Node node, WalkTreeCallback callback)
-		{
-			callback(node);
-			foreach (Node child in node.GetChildren())
-				WalkTree(child, callback);
-		}
+        public static void WalkTree(Node root, WalkTreeCallback callback)
+        {
+            callback(root);
+            foreach (Node child in root.GetChildren())
+                WalkTree(child, callback);
+        }
 
-		public static void PrintTree(Node node)
-		{
-			PrintTree(node, (currNode) => true);
-		}
+        public static void PrintTree(Node root) => PrintTree(root, "", false);
+        private static void PrintTree(Node root, string prefix, bool replaceLast)
+        {
+            GD.Print($"{prefix}--> {root.Name}");
+            if (replaceLast)
+                prefix = prefix.Remove(prefix.Length - 1, 1) + " ";
+            if (root.GetChildCount() == 0)
+                return;
+            for (int i = 0; i < root.GetChildCount() - 1; i++)
+                PrintTree(root.GetChild(i), $"{prefix} |", false);
+            PrintTree(root.GetChild(root.GetChildCount() - 1), $"{prefix} '", true);
+        }
 
-		public static void PrintTree(Node node, PrintTreeCondition condition)
-		{
-			WalkTree(node, (currNode) =>
-			{
-				if (condition(currNode))
-					GD.Print(String.Format("Found Node at: \"{0}\"", currNode.GetPath()));
-			});
-		}
-	}
+        public static void FindNodes(Node root)
+        {
+            FindNodes(root, (currNode) => true);
+        }
+
+        public static void FindNodes(Node root, PrintTreeCondition condition)
+        {
+            WalkTree(root, (currNode) =>
+            {
+                if (condition(currNode))
+                    GD.Print(String.Format("Found Node at: \"{0}\"", currNode.GetPath()));
+            });
+        }
+    }
 }
--- Utils/EditorUtils.cs
+++ Utils/EditorUtils.cs
@@ -5,44 +5,44 @@ using System.Text.RegularExpressions;
 
 namespace Fractural.Utils
 {
-	public static class EditorUtils
-	{
-		public static Texture GetIconRecursive<T>(this Control controlNode)
-		{
-			return GetIconRecursive(controlNode, typeof(T));
-		}
+    public static class EditorUtils
+    {
+        public static Texture GetIconRecursive<T>(this Control controlNode)
+        {
+            return GetIconRecursive(controlNode, typeof(T));
+        }
 
-		public static Texture GetIconRecursive(this Control controlNode, object obj)
-		{
-			Type godotBaseType = obj.GetType();
-			while (godotBaseType.Namespace?.Split(".").FirstOrDefault() != nameof(Godot) && godotBaseType != null)
-				godotBaseType = godotBaseType.BaseType;
-			return controlNode.GetIcon(godotBaseType.Name, "EditorIcons");
-		}
+        public static Texture GetIconRecursive(this Control controlNode, object obj)
+        {
+            Type godotBaseType = obj.GetType();
+            while (godotBaseType.Namespace?.Split(".").FirstOrDefault() != nameof(Godot) && godotBaseType != null)
+                godotBaseType = godotBaseType.BaseType;
+            return controlNode.GetIcon(godotBaseType.Name, "EditorIcons");
+        }
 
-		public static Type GetRealType(Node node)
-		{
-			var attachedCSharpScript = ((CSharpScript)node.GetScript());
-			if (attachedCSharpScript != null)
-			{
-				File file = new File();
-				if (file.Open(attachedCSharpScript.ResourcePath, File.ModeFlags.Read) == Error.Ok)
-				{
-					Regex regex = new Regex(@"namespace ([\w.]+)");
-					var match = regex.Match(file.GetAsText());
-					file.Close();
-					var namespacePrefix = "";
-					if (match.Success && match.Groups.Count > 1)
-					{
-						// First matching group is the namespace of this class
-						namespacePrefix = match.Groups[1].Value + ".";
-					}
-					var type = Type.GetType(namespacePrefix + attachedCSharpScript.ResourcePath.GetFileName());
-					if (type != null)
-						return type;
-				}
-			}
-			return node.GetType();
-		}
-	}
+        public static Type GetRealType(Node node)
+        {
+            var attachedCSharpScript = ((CSharpScript)node.GetScript());
+            if (attachedCSharpScript != null)
+            {
+                File file = new File();
+                if (file.Open(attachedCSharpScript.ResourcePath, File.ModeFlags.Read) == Error.Ok)
+                {
+                    Regex regex = new Regex(@"namespace ([\w.]+)");
+                    var match = regex.Match(file.GetAsText());
+                    file.Close();
+                    var namespacePrefix = "";
+                    if (match.Success && match.Groups.Count > 1)
+                    {
+                        // First matching group is the namespace of this class
+                        namespacePrefix = match.Groups[1].Value + ".";
+                    }
+                    var type = Type.GetType(namespacePrefix + attachedCSharpScript.ResourcePath.GetFileName());
+                    if (type != null)
+                        return type;
+                }
+            }
+            return node.GetType();
+        }
+    }
 }
