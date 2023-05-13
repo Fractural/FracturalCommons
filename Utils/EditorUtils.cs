a2b4d3a13cdb1ce0947ce32c35b236919004d2ed
Atlinx <atlinuxtm@gmail.com>
2023-05-13 22:53:16 +0000

    Update release to output generated at b2ed959

--- Utils/EditorUtils.cs
+++ Utils/EditorUtils.cs
@@ -21,29 +21,33 @@ namespace Fractural.Utils
             return controlNode.GetIcon(godotBaseType.Name, "EditorIcons");
         }
 
-        public static Type GetRealType(Node node)
+        public static Type GetCSharpType(this Node node)
         {
             var attachedCSharpScript = ((CSharpScript)node.GetScript());
             if (attachedCSharpScript != null)
-            {
-                File file = new File();
-                if (file.Open(attachedCSharpScript.ResourcePath, File.ModeFlags.Read) == Error.Ok)
-                {
-                    Regex regex = new Regex(@"namespace ([\w.]+)");
-                    var match = regex.Match(file.GetAsText());
-                    file.Close();
-                    var namespacePrefix = "";
-                    if (match.Success && match.Groups.Count > 1)
-                    {
-                        // First matching group is the namespace of this class
-                        namespacePrefix = match.Groups[1].Value + ".";
-                    }
-                    var type = Type.GetType(namespacePrefix + attachedCSharpScript.ResourcePath.GetFileName());
-                    if (type != null)
-                        return type;
-                }
-            }
+                return GetCSharpType(attachedCSharpScript.ResourcePath);
             return node.GetType();
+        }
+
+        public static Type GetCSharpType(string filePath)
+        {
+            File file = new File();
+            if (file.Open(filePath, File.ModeFlags.Read) == Error.Ok)
+            {
+                Regex regex = new Regex(@"namespace ([\w.]+)");
+                var match = regex.Match(file.GetAsText());
+                file.Close();
+                var namespacePrefix = "";
+                if (match.Success && match.Groups.Count > 1)
+                {
+                    // First matching group is the namespace of this class
+                    namespacePrefix = match.Groups[1].Value + ".";
+                }
+                var type = Type.GetType(namespacePrefix + filePath.GetFileName());
+                if (type != null)
+                    return type;
+            }
+            return null;
         }
 
 #if TOOLS
