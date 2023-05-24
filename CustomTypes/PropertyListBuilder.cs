47319e07a1ba74c5549d8ed871899a065ed47ef7
Atlinx <atlinuxtm@gmail.com>
2023-05-24 00:59:43 +0000

    Update release to output generated at d716a7e

--- CustomTypes/PropertyListBuilder.cs
+++ CustomTypes/PropertyListBuilder.cs
@@ -1,35 +1,36 @@
 ﻿using Godot;
 using System.Collections.Generic;
+using System.Linq;
 using GDC = Godot.Collections;
 
 namespace Fractural
 {
-	public class PropertyListBuilder
-	{
-		public List<PropertyListItem> Items { get; } = new List<PropertyListItem>();
-		public GDC.Array PropertyArray { get; private set; }
+    public class PropertyListBuilder
+    {
+        public List<PropertyListItem> Items { get; } = new List<PropertyListItem>();
+        public GDC.Array PropertyArray { get; private set; }
 
-		public PropertyListBuilder() : this(new GDC.Array()) { }
-		public PropertyListBuilder(GDC.Array propertyArray)
-		{
-			PropertyArray = propertyArray != null ? propertyArray : new GDC.Array();
-		}
+        public PropertyListBuilder() : this(new GDC.Array()) { }
+        public PropertyListBuilder(GDC.Array propertyArray)
+        {
+            PropertyArray = propertyArray != null ? propertyArray : new GDC.Array();
+        }
 
-		public void AddItem(PropertyListItem item)
-		{
-			Items.Add(item);
-		}
+        public void AddItem(PropertyListItem item)
+        {
+            Items.Add(item);
+        }
 
-		public void AddItem(string name, Variant.Type type, PropertyHint hint = PropertyHint.None, string hintString = "", PropertyUsageFlags usage = PropertyUsageFlags.Default)
-		{
-			AddItem(new PropertyListItem(name, type, hint, hintString, usage));
-		}
+        public void AddItem(string name, Variant.Type type, PropertyHint hint = PropertyHint.None, string hintString = "", PropertyUsageFlags usage = PropertyUsageFlags.Default)
+        {
+            AddItem(new PropertyListItem(name, type, hint, hintString, usage));
+        }
 
-		public GDC.Array Build()
-		{
-			foreach (var item in Items)
-				PropertyArray.Add(item.ToGDDict());
-			return PropertyArray;
-		}
-	}
+        public GDC.Array Build()
+        {
+            foreach (var item in Items)
+                PropertyArray.Add(item.ToGDDict());
+            return PropertyArray;
+        }
+    }
 }
