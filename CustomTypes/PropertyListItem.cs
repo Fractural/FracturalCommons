b97fc09394da5a08ff62e4ff7c48bb18f5223c87
Atlinx <atlinuxtm@gmail.com>
2023-05-26 02:07:57 +0000

    Update release to output generated at 6433b4a

--- CustomTypes/PropertyListItem.cs
+++ CustomTypes/PropertyListItem.cs
@@ -1,4 +1,6 @@
-﻿using Godot;
+﻿using Fractural.Utils;
+using Godot;
+using GDC = Godot.Collections;
 
 namespace Fractural
 {
@@ -19,15 +21,27 @@ namespace Fractural
             this.usage = usage;
         }
 
-        public Godot.Collections.Dictionary ToGDDict()
+        public GDC.Dictionary ToGDDict()
         {
-            return new Godot.Collections.Dictionary
+            return new GDC.Dictionary
             {
                 ["name"] = name,
                 ["type"] = type,
+                ["hint"] = hint,
                 ["hint_string"] = hintString,
                 ["usage"] = usage
             };
+        }
+
+        public static PropertyListItem FromGDDict(GDC.Dictionary dict)
+        {
+            return new PropertyListItem(
+                name: dict.Get<string>("name"),
+                type: dict.Get<Variant.Type>("type"),
+                hint: (PropertyHint)dict.Get<int>("hint", 0),
+                hintString: dict.Get<string>("hint_string", ""),
+                usage: (PropertyUsageFlags)dict.Get<int>("usage", 0)
+            );
         }
     };
 }
