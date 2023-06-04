b14e06eda2311bd305693687b3b17323080b84f8
Atlinx <atlinuxtm@gmail.com>
2023-06-04 23:48:48 +0000

    Update release to output generated at 45a07a9

--- Utils/GeneralUtils.cs
+++ Utils/GeneralUtils.cs
@@ -9,5 +9,15 @@ namespace Fractural.Utils
         {
             return ((h1 << 5) + h1) ^ h2;
         }
+
+        public static int CombineHashCodes(params object[] objects) 
+        {
+            if (objects == null || objects.Length == 0)
+                return 0;
+            int code = objects[0].GetHashCode();
+            for (int i = 1; i < objects.Length; i++)
+                code = ((code << 5) + code) ^ (objects[i]?.GetHashCode() ?? 0);
+            return code;
+        }
     }
 }
