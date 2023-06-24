9a09e27ae87dd483f30f4c30504284c1db76bf71
atlinx <atlinuxtm@gmail.com>
2023-06-24 19:58:00 -0400

    Make CombineHashCodes work with collections and enumerables

--- addons/FracturalCommons/Utils/GeneralUtils.cs
+++ addons/FracturalCommons/Utils/GeneralUtils.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Linq;
 
 namespace Fractural.Utils
@@ -10,13 +11,29 @@ namespace Fractural.Utils
             return ((h1 << 5) + h1) ^ h2;
         }
 
-        public static int CombineHashCodes(params object[] objects) 
+        public static int CombineHashCodes(params object[] objects)
         {
             if (objects == null || objects.Length == 0)
                 return 0;
-            int code = objects[0].GetHashCode();
+            int code = GetHashCode(objects[0]);
             for (int i = 1; i < objects.Length; i++)
-                code = ((code << 5) + code) ^ (objects[i]?.GetHashCode() ?? 0);
+                code = ((code << 5) + code) ^ GetHashCode(objects[i]);
+            return code;
+        }
+
+        public static int GetHashCode(object obj)
+        {
+            int code = 0;
+            if (obj is IDictionary dictionary)
+            {
+                foreach (var key in dictionary.Keys)
+                    code = CombineHashCodes(code, dictionary[key]);
+            }
+            else if (obj is IEnumerable enumerable)
+            {
+                foreach (var elem in enumerable)
+                    code = CombineHashCodes(code, elem.GetHashCode());
+            }
             return code;
         }
     }
