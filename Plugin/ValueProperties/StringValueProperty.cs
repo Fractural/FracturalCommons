5d4bb4cacc58229545896927ca135cd1d9f97128
Atlinx <atlinuxtm@gmail.com>
2023-06-25 19:56:18 +0000

    Update release to output generated at da913dc

--- Plugin/ValueProperties/StringValueProperty.cs
+++ Plugin/ValueProperties/StringValueProperty.cs
@@ -6,24 +6,25 @@ namespace Fractural.Plugin
     [Tool]
     public class StringValueProperty : ValueProperty<string>
     {
-        public string PlaceholderText 
+        public string PlaceholderText
         {
             get => _lineEdit.PlaceholderText;
             set => _lineEdit.PlaceholderText = value;
         }
 
-        public Font Font 
+        public Font Font
         {
             get => _lineEdit.GetFont("font");
             set => _lineEdit.AddFontOverride("font", value);
         }
-        
+
         private LineEdit _lineEdit;
 
         public StringValueProperty() : base()
         {
             _lineEdit = new LineEdit();
             _lineEdit.SizeFlagsHorizontal = (int)SizeFlags.ExpandFill;
+            _lineEdit.AddConstantOverride("minimum_spaces", 0);
             _lineEdit.Connect("text_entered", this, nameof(OnTextEntered));
             _lineEdit.Connect("focus_exited", this, nameof(OnFocusExited));
             AddChild(_lineEdit);
